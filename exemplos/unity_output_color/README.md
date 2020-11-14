6ee727478f63f2ae6c61cf223fb0bca72b5ebf8f
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-14 20:36:27 -0300

    Exemplos de uso da macro UNITY_OUTPUT_COLOR
    A definição desta macro habilita as cores nas mensagens de erro do unity

---
+++ exemplos/unity_output_color/README.md
@@ -0,0 +1,8 @@
+#### Exemplo exibir mensagem colorida do Unity
+
+Para habilitar a saída colorida das mensagens de erro no Unity, acrescente a
+seguinte linha na linha 10 em Unity/unity.h:
+
+```c
+#define UNITY_OUTPUT_COLOR
+```
---
+++ exemplos/unity_output_color/teste.c
@@ -0,0 +1,23 @@
+/*
+* teste.c (C)
+* 
+* Objetivo: Exemplo exibir mensagem colorida no C.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 14/11/2020
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
+#include <stdio.h>
+
+int main(void){
+
+	printf("\033[31m olá galera do canal Geofisicando!\033[m \n");
+}
