703d59491aa6b481536383ee0a4434848778c0ec
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-03-26 16:01:51 -0300

    Exemplo de implementação de uma fila em C

---
+++ exemplos/fila/fila.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct sPessoa{
+	int idade;
+	char nome[20+1];
+	struct sPessoa *prox;
+} PESSOA;
+
+typedef PESSOA *FILA;
+
+void criar(FILA* f){
+	*f = NULL;
+}
+
+void inserir(FILA* f, int idade, const char* nome){
+	
+	if(*f==NULL){
+		*f = (FILA) malloc(sizeof(PESSOA));
+		if(*f==NULL) return;
+		(*f)->idade=idade;
+		strcpy((*f)->nome,nome);
+		(*f)->prox=NULL;
+	}else{
+		inserir(&(**f).prox,idade,nome);
+	}
+}
+
+void listar(FILA f){
+	if(f==NULL) return;
+	printf("%s %d\n",f->nome,f->idade);
+	listar(f->prox);
+}
+
+void apagar(FILA* f){
+	FILA tmp = *f;
+	if(*f==NULL) return;
+	*f = (*f)->prox;
+	free(tmp);
+}
+
+int main(void){
+	FILA f;
+	criar(&f);
+	inserir(&f,29,"Dirack");
+	inserir(&f,44,"Fulano");
+	inserir(&f,22,"Beltrano");
+	listar(f);
+	puts("**********");
+	apagar(&f);
+	listar(f);
+}
