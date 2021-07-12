2cdfff9d00b6d3ea2b17e068ff5fae84a319f11d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-12 17:49:58 -0300

    exemplo

--- exemplos/bibliotecas_estaticas/exemplo/_alloc.c
+++ exemplos/bibliotecas_estaticas/exemplo/_alloc.c
@@ -1,3 +1,21 @@
+/*
+* _alloc.c (C)
+* 
+* Objetivo: Biblioteca genérica para o exemplo de criação
+* de uma biblioteca estática em C.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 12/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
