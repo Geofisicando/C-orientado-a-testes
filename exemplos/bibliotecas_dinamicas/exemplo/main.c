a00a0b2e5455623e9cd07ef2cd5c244839c1a8cb
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-12 17:56:46 -0300

    exemplo

--- exemplos/bibliotecas_dinamicas/exemplo/main.c
+++ exemplos/bibliotecas_dinamicas/exemplo/main.c
@@ -1,3 +1,20 @@
+/*
+* main.c (C)
+* 
+* Objetivo: Programa principal que utilizará a biblioteca dinâmica do exemplo.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 12/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include "_alloc.h"
 
 int main(void){
