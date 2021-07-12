fc8380f5f1579c3e33c17a1642e5a2bba666842f
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-12 18:00:31 -0300

    exemplo

--- exemplos/hipotenusa/funcao_hipotenusa.h
+++ exemplos/hipotenusa/funcao_hipotenusa.h
@@ -1,3 +1,20 @@
+/*
+* funcao_hipotenusa.h (C)
+* 
+* Objetivo: Função hipotenusa para teste.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 12/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
--- exemplos/hipotenusa/test_hipotenusa.c
+++ exemplos/hipotenusa/test_hipotenusa.c
@@ -1,3 +1,20 @@
+/*
+* test_hipotenusa.c (C)
+* 
+* Objetivo: Suíte de testes da função hipotenusa.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 12/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include "Unity/unity.h"
 #include "funcao_hipotenusa.h"
 
