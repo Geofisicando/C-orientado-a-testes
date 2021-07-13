58c52b8e578309dde21662d06ca0deb246a9a766
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-13 11:56:18 -0300

    Exemplo

--- exemplos/hipotenusa_refatorada/funcao_hipotenusa.h
+++ exemplos/hipotenusa_refatorada/funcao_hipotenusa.h
@@ -1,3 +1,19 @@
+/*
+* funcao_hipotenusa.h (C)
+* 
+* Objetivo: Função hipotenusa do exemplo de refatoração.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 13/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <math.h>
 
 float hipotenusa(float cateto_a, float cateto_b){
--- exemplos/hipotenusa_refatorada/test_hipotenusa.c
+++ exemplos/hipotenusa_refatorada/test_hipotenusa.c
@@ -1,3 +1,19 @@
+/*
+* test_hipotenusa.c (C)
+* 
+* Objetivo: Suíte de teste da função hipotenusa refatorada.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 13/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include "Unity/unity.h"
 #include "funcao_hipotenusa.h"
 
