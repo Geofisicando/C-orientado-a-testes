0ef7f3d153686526356cdad7734497d7c8e26652
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-14 13:30:14 -0300

    exemplo

--- exemplos/string_sep/exemplo.c
+++ exemplos/string_sep/exemplo.c
@@ -1,3 +1,19 @@
+/*
+* exemplo.c (C)
+* 
+* Objetivo: Função separadora de strings na forma 'chave=valor'.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 14/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
