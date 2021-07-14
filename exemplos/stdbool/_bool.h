df83a939333c59a7ee9db64db4e2a62eac31a1a0
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-14 12:08:09 -0300

    exemplo bool

--- exemplos/stdbool/_bool.h
+++ exemplos/stdbool/_bool.h
@@ -1,4 +1,19 @@
+/*
+* _bool.h (C)
+* 
+* Objetivo: Função bool personalizada.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 14/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 typedef enum {false,true} BOOL;
 
 typedef BOOL bool;
-
--- exemplos/stdbool/test_bool.c
+++ exemplos/stdbool/test_bool.c
@@ -1,3 +1,19 @@
+/*
+* test_bool.c (C)
+* 
+* Objetivo: Teste da função bool personalizada.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 14/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include "Unity/unity.h"
 #include "_bool.h"
 
