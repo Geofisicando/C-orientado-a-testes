4f0cbf679332a0a41f76a73944d4aaae9a4f1598
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-18 10:08:29 -0300

    corrige bug matriz simétrica

---
+++ exemplos/matrizSimetrica/corrigir_bug/matriz.c
@@ -0,0 +1,65 @@
+/*
+* matriz.c (C)
+* 
+* Objetivo: Implementação das funções do programa matriz simétrica.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 17/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
+ 
+MatrizSimetrica* cria (int n) {
+    int s = n*(n+1)/2;
+    MatrizSimetrica* mat = (MatrizSimetrica*) malloc(sizeof(MatrizSimetrica));
+    mat->dim = n;
+    mat->v = (float*) malloc(s*sizeof(float));
+    return mat;
+}
+ 
+void libera (MatrizSimetrica* mat) {
+    free(mat->v);
+    free(mat);
+}
+ 
+float acessa (MatrizSimetrica* mat, int i, int j) {
+    int k; /* indice do elemento no vetor */
+    if (i<0 || i>=mat->dim || j<0 || j>=mat->dim) {
+        printf("\nAcesso invalido.\n");
+        exit(1);
+    }
+    if (i>=j)
+        k = i*(i+1)/2 + j;
+    else
+        k = j*(j+1)/2 + i;
+
+    printf("%s: i=%d j=%d k=%d\n",__FUNCTION__,i,j,k);
+    return mat->v[k];
+}
+ 
+void atribui (MatrizSimetrica* mat, int i, int j, float v) {
+    int k;
+    if (i<0 || i>=mat->dim || j<0 || j>=mat->dim) {
+        printf("\nAtribuicao invalida.\n");
+        exit(1);
+    }
+
+    if (i>=j)
+        k = i*(i+1)/2 + j;
+    else
+        k = j*(j+1)/2 + i;
+
+    mat->v[k] = v;
+
+    printf("%s: i=%d j=%d k=%d\n",__FUNCTION__,i,j,k);
+}
+ 
---
+++ exemplos/matrizSimetrica/corrigir_bug/test_matriz.c
@@ -0,0 +1,54 @@
+/*
+* test_matriz.c (C)
+* 
+* Objetivo: Testes de funções matriz simétrica.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves 17/07/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include "Unity/unity.h"
+#include "matriz.h"
+
+void setUp(){};
+
+void tearDown(){};
+
+void test_matriz_simetrica_2_por_2(){
+/* A matriz utilizada será:
+ *
+ * 	1 2 
+ * 	2 1 
+ */
+
+	int i;
+
+	MatrizSimetrica* mp = cria(2);
+
+	atribui(mp,0,0,1.0);
+	atribui(mp,0,1,2.0);
+	atribui(mp,1,0,2.0);
+	atribui(mp,1,1,1.0);
+
+	for(i=0;i<3;i++)
+		printf("%f\n",mp->v[i]);
+
+	TEST_ASSERT_FLOAT_WITHIN(0.01,1.0,acessa(mp,0,0));
+	TEST_ASSERT_FLOAT_WITHIN(0.01,2.0,acessa(mp,0,1));
+	TEST_ASSERT_FLOAT_WITHIN(0.01,2.0,acessa(mp,1,0));
+	TEST_ASSERT_FLOAT_WITHIN(0.01,1.0,acessa(mp,1,1));
+
+};
+
+int main(){
+	UNITY_BEGIN();
+	RUN_TEST(test_matriz_simetrica_2_por_2);
+	return UNITY_END();
+}
