85c6ac89502e9f7b5f32b2ec4a9fb5cedf178947
Dirack <rodolfo_profissional@hotmail.com>
2021-08-19 10:06:51 -0300

    aula 32

--- exemplos/GDB/bug_funcao/funcoes.h
+++ exemplos/GDB/bug_funcao/funcoes.h
@@ -1,3 +1,18 @@
+/*
+* funcoes.h (C)
+* 
+* Objetivo: Exemplos de funções com BUGs para testar comandos do GDB.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 19/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
 
 float soma(float f1, float f2){
 	return f1*f2;
--- exemplos/GDB/bug_funcao/main.c
+++ exemplos/GDB/bug_funcao/main.c
@@ -1,3 +1,19 @@
+/*
+* main.c (C)
+* 
+* Objetivo: Exemplo de programa com BUGs e funções para testar comandos do GDB.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 19/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <stdio.h>
 #include "funcoes.h"
 
