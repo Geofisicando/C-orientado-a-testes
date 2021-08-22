d6870bd45fcb7e591859e1e001fc721fcc8d5ff5
Dirack <rodolfo_profissional@hotmail.com>
2021-08-22 12:22:08 -0300

    aula display

---
+++ exemplos/GDB/display/main.c
@@ -0,0 +1,29 @@
+/*
+* main.c (C)
+* 
+* Objetivo: Exemplo de programa simples com laço for para testar os comandos do gdb.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 22/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+	int i;
+	int j=1;
+
+	for(i=0;i<10;i++){
+
+		j = i+j;
+
+	}
+}
