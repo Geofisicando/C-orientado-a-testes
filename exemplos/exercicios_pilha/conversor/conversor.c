fd878ff230e2b343f4e965bd0862dbe62406ac90
Dirack <rodolfo_profissional@hotmail.com>
2021-08-27 16:20:35 -0300

    pilha conversor

--- exemplos/exercicios_pilha/conversor/conversor.c
+++ exemplos/exercicios_pilha/conversor/conversor.c
@@ -1,3 +1,20 @@
+/*
+* conversor.c (C)
+* 
+* Objetivo: Conversor de número inteiro decimal para binário.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 27/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include <stdio.h>
 #include "pilha.h"
 
--- exemplos/exercicios_pilha/conversor/pilha.h
+++ exemplos/exercicios_pilha/conversor/pilha.h
@@ -1,3 +1,20 @@
+/*
+* pilha.h (C)
+* 
+* Objetivo: Implementação de uma pilha de números inteiros.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 27/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+
 #include <stdlib.h>
 
 struct sNo{
