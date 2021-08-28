afe2943b1bb848fd0d1c788d8d85d7e5bba5c081
Dirack <rodolfo_profissional@hotmail.com>
2021-08-28 12:57:08 -0300

    exercício pilha pizzas

--- exemplos/exercicios_pilha/sistema_entregas_pizza/sistema.c
+++ exemplos/exercicios_pilha/sistema_entregas_pizza/sistema.c
@@ -1,3 +1,20 @@
+/*
+* sistema.c (C)
+* 
+* Objetivo: Implementação do sistema de entrega de pizzas
+* utilizando a estrutura de dados pilha.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 28/08/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "pilha.h"
