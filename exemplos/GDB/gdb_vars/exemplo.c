305985627a0f5b5cfdf7bc1caf1a36c0b3032a6a
Dirack <rodolfo_profissional@hotmail.com>
2021-09-03 19:15:36 -0300

    Variáveis no GDB

--- exemplos/GDB/gdb_vars/exemplo.c
+++ exemplos/GDB/gdb_vars/exemplo.c
@@ -1,3 +1,19 @@
+/*
+* exemplo.c (C)
+* 
+* Objetivo: Exemplo de utilização de variáveis no GDB.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 03/09/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
