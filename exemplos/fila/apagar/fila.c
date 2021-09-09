0134826e912f5dff3465c41ffa832277c14839d3
Dirack <rodolfo_profissional@hotmail.com>
2021-09-09 18:45:52 -0300

    apagar fila

---
+++ exemplos/fila/apagar/fila.c
@@ -0,0 +1,69 @@
+/*
+* fila.c (C)
+* 
+* Objetivo: Implementação da estrutura de dados fila.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 09/09/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct sPessoa{
+	int idade;
+	char nome[20+1];
+	struct sPessoa *prox;
+} PESSOA;
+
+typedef PESSOA *FILA;
+
+void criar(FILA* f){
+	*f = NULL;
+}
+
+void inserir(FILA* f, int idade, const char* nome){
+	
+	if(*f==NULL){
+		*f = (PESSOA*) malloc(sizeof(PESSOA));
+		if(*f==NULL) return;
+		(*f)->idade=idade;
+		strcpy((*f)->nome,nome);
+		(*f)->prox=NULL;
+	}else{
+		inserir(&(**f).prox,idade,nome);
+	}
+}
+
+void listar(FILA f){
+	if(f==NULL) return;
+	printf("%s %d\n",f->nome,f->idade);
+	listar(f->prox);
+}
+
+void apagar(FILA* f){
+	FILA tmp = *f;
+	if(*f==NULL) return;
+	*f = (*f)->prox;
+	free(tmp);
+}
+
+int main(void){
+	FILA f;
+	criar(&f);
+	inserir(&f,29,"Dirack");
+	inserir(&f,44,"Fulano");
+	inserir(&f,22,"Beltrano");
+	listar(f);
+	puts("**********");
+	apagar(&f);
+	listar(f);
+}
