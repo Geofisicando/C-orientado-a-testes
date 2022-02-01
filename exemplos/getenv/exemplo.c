c9d4936d4b24ed07ab081c7df3c4c1a6d0f76b9b
Dirack <rodolfo_profissional@hotmail.com>
2022-02-01 10:51:44 -0300

    getenv

--- exemplos/getenv/exemplo.c
+++ exemplos/getenv/exemplo.c
@@ -1,3 +1,19 @@
+/*
+* exemplo.c (C)
+* 
+* Objetivo: Exemplo de uso da função getenv.
+* 
+* Site: https://dirack.github.io
+* 
+* Versão 1.0
+* 
+* Programador: Rodolfo A C Neves (Dirack) 01/02/2021
+* 
+* Email: rodolfo_profissional@hotmail.com
+* 
+* Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
